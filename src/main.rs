d70eb6c360bd80c724137b87835c5885e5fc5e55
Lily Hopkins <bsalarius@gmail.com>
2019-11-06 17:17:58 +0000

    First commit

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use reqwest;
+use regex::Regex;
+use metar::Metar;
+
+#[derive(Debug)]
+enum MTError {
+    RequestError(reqwest::Error),
+    MetarParseError(metar::MetarError),
+}
+
+impl From<reqwest::Error> for MTError {
+    fn from(e: reqwest::Error) -> Self {
+        Self::RequestError(e)
+    }
+}
+
+impl From<metar::MetarError> for MTError {
+    fn from(e: metar::MetarError) -> Self {
+        Self::MetarParseError(e)
+    }
+}
+
+fn get_metar(station: &str) -> Result<String, MTError> {
+    let body = reqwest::get(&format!("https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&hoursBeforeNow=3&mostRecent=true&stationString={}", station))?
+        .text()?;
+
+    let re = Regex::new(r"<raw_text>([^<]+)</raw_text>").unwrap();
+    let caps = re.captures(&body).unwrap();
+    let metar_text = &caps[1];
+    Ok(metar_text.to_string())
+}
+
+fn main() {
+    let test_stations = ["KLAX", "EGSS", "LTBJ", "EDDK", "EGMC", "EGGD", "ESSA", "RJAA"];
+
+    for station in &test_stations {
+        let res = get_metar(station);
+        if let Ok(metar) = res {
+            println!("Testing METAR: {}", metar.clone());
+            let r = Metar::parse(metar.clone());
+            if let Err(e) = r {
+                eprintln!("Error parsing: {}", metar);
+                eprintln!("{:#?}", e);
+                eprintln!("-----");
+            }
+        } else {
+            let res = res.unwrap_err();
+            eprintln!("Error fetching data for testing {}", station);
+            eprintln!("{:#?}", res);
+            eprintln!("-----");
+        }
+    }
+}
